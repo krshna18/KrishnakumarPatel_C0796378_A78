108ef2dc34ba3b1a362e12ebec7528a678632be9
Krishna_1809 <krishna.patel@alphatechsoftware.com>
2021-03-28 11:16:32 -0400

    Initial commit

---
+++ src/com/krishna/Assignment7.java
@@ -0,0 +1,57 @@
+package com.krishna;
+
+import java.util.Scanner;
+
+public class Assignment7 {
+    public static void main(String[] args){
+        String user[] = {"Krishna", "Parth", "Shivangi", "Yash"};
+        String usercredentils[] = {"KP", "PP", "SR", "YP"};
+        Scanner scanner = new Scanner(System.in);
+        Account obj1 = new Account();
+        String password;
+        int option;
+
+        for (int i=0; i<user.length; i++){
+            System.out.println("Enter Credentials for user " + user[i]);
+            password = scanner.next();
+            if(password.equals(usercredentils[i])) {
+                boolean blnContinue = true;
+                while(blnContinue != false)
+                {
+                    System.out.println("press 1 for withdrawal");
+                    System.out.println("press 2 for deposit");
+                    System.out.println("press 3 to view balance");
+                    option = scanner.nextInt();
+                    double amount;
+                    switch (option){
+                        case 1:
+                            amount = scanner.nextDouble();
+                            obj1.withdrawal(amount);
+                            break;
+                        case 2:
+                            amount = scanner.nextDouble();
+                            obj1.deposit(amount);
+                            break;
+                        case 3:
+                            double balance;
+                            balance = obj1.getBalance();
+                            System.out.println("Balance is: " +balance);
+                            break;
+                        default:
+                            System.out.println("Please select form the given option");
+                            break;
+                    }
+                    System.out.println("do you want to continue banking for " + user[i]+ "? yes or no");
+                    String result = scanner.next();
+                    if (result.equals("yes")){
+                        blnContinue = true;
+                    } else {
+                        blnContinue = false;
+                    }
+                }
+            } else {
+                System.out.println("Invalid user password");
+            }
+        }
+    }
+}
